5dc51118efab1855a6aa4198f74d3f7344ee0b96
Andy <Andy.Makhoka@lmdsio.fr>
2023-03-09 17:04:16 +0100

    Via le lycée

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\VisiteurController;
+use \App\Http\Controllers\FraisController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/seConnecter', [VisiteurController::class, 'getLogin']);
+Route::post('/login', [VisiteurController::class , 'signIn']);
+Route::get('/seDeconnecter', [VisiteurController::class, 'signOut']);
+
+Route::get('/getListeFrais', [FraisController::class, 'getFraisVisiteur']);
+Route::get('/listerFrais', [FraisController::class, 'getFraisVisiteur']);
+
+
+Route::get('/modifierFrais/{id}', [FraisController::class, 'updateFrais']);
+Route::post('/validerFrais', [FraisController::class, 'validateFrais']);
+
+
+Route::get('/ajouterFrais', [FraisController::class, 'addFrais']);
+Route::post('/validerFrais', [FraisController::class, 'validateFrais']);
+
+Route::get('/supprimerFrais/{id}', [FraisController::class, 'supprimeFrais']);
+
+//Frais Hors Forfait
+Route::get('/listeFraisHorsForfait/{id}', [FraisController::class, 'getFraisVisiteurHorsForfait']);
+
+Route::get('/modifierFraisHorsForfait/{id}', [FraisController::class, 'updateFraisHosForfait']);
+Route::post('/validerFraisHorsForfait', [FraisController::class, 'validateFraisHorsForfait']);
+
+Route::get('/ajouterFraisHorsForfait/{id}', [FraisController::class, 'addFraisHorsForfait']);
+Route::post('/validerFraisHorsForfait/{id}', [FraisController::class, 'validateFraisHorsForfait']);
