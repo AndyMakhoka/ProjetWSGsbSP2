8a0d40e1c1d29d935ea80db3d99577ed8f591add
andy <Andy.Makhoka2@gmail.com>
2023-06-13 18:48:11 +0200

    Optimisation - from home

--- resources/views/vues/error.blade.php
+++
@@ -1,5 +0,0 @@
-@if($erreur != "")
-            <div class="alert-danger" role="alert">
-                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>{{$erreur}}
-            </div>
-@endif
